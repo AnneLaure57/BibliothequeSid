6850e430824f74cfa30e5d894877e736b47f4d14
AnneLaure57 <>
2020-12-08 19:11:56 +0100

    Code : remove getters + setters, use @Data

--- src/main/java/fr/miage/sid/bibliothequeCharlesYacia/objets_metiers_de_la_bibliotheque/Usager.java
+++ src/main/java/fr/miage/sid/bibliothequeCharlesYacia/objets_metiers_de_la_bibliotheque/Usager.java
@@ -19,6 +19,7 @@ import javax.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Entity // Pour associer la classe à une table dans la base
 @Data
 @NoArgsConstructor
@@ -69,102 +70,6 @@ public class Usager {
 
     @OneToMany(mappedBy = "usager", cascade = CascadeType.ALL)
     private List<Reservation> reservations = new ArrayList<>();
-	
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int ID) {
-        this.id = ID;
-    }
-    
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public String getAdresse() {
-		return adresse;
-	}
-
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
-
-	public int getCodepostal() {
-		return codepostal;
-	}
-
-	public void setCodepostal(int codepostal) {
-		this.codepostal = codepostal;
-	}
-
-	public String getVille() {
-		return ville;
-	}
-
-	public void setVille(String ville) {
-		this.ville = ville;
-	}
-
-	public String getTelephone() {
-		return telephone;
-	}
-
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-	
-	public String getMail() {
-		return mail;
-	}
-
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-
-	public Date getDateCreation() {
-		return dateCreation;
-	}
-
-	public void setDateCreation(Date dateCreation) {
-		this.dateCreation = dateCreation;
-	}
-
-	public Date getDateNaissance() {
-		return dateNaissance;
-	}
-
-	public void setDateNaissance(Date dateNaissance) {
-		this.dateNaissance = dateNaissance;
-	}
-
-	public Date getDateArchivage() {
-		return dateArchivage;
-	}
-
-	public void setDateArchivage(Date dateSuppression) {
-		this.dateArchivage = dateSuppression;
-	}
-
-	public Usager e_identification(String nom) {
-		throw new UnsupportedOperationException();
-	}
-
-	public void supprimerUsager(Usager usager) {
-		throw new UnsupportedOperationException();
-	}
 
 	public Usager(String nom, String prenom, String adresse, int codepostal, String ville, String telephone, String mail,
 			 Date dateNaissance) {
